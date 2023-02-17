932c19a01d43bcc8f68bd1a923121a77d724dc38
EvaDM <evamorante1969@gmail.com>
2023-02-17 15:51:14 +0100

    changts corrections et titre sur l'index dynamic interface

--- 47.DynamicUserInterface.js
+++ 47.DynamicUserInterface.js
@@ -1,11 +1,20 @@
-const p = document.getElementsByTagName ("p");
-const textBox = document.getElementbyId ("textbox");
-const button = document.getElementbyId ("button");
 
-function fullParagraphe() {
-    textBox.innerText = textbox.value;
+//a paragraph of text which should be empty 
+//when the page is first opened
+//a textbox
+//a button
+//When the button is clicked, the paragraph of text will 
+//display what is currently in the textbox
+
+
+const p = document.getElementById ("1");
+const textbox = document.getElementById ("textbox");
+const button = document.getElementById ("button");
+
+function fullingParagraphe() {
+    p.innerText = textbox.value;
     
 }
-fullParagraphe.addEventListener ("click", fullParagraphe);
+button.addEventListener("click", fullingParagraphe);
 
 //check la correction !
