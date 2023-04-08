e60ae7ff876a6a48bb32398874be0a33abfd4b1b
EvaDM <evamorante1969@gmail.com>
2023-04-08 23:08:52 +0200

    coding for converter of temp in javascript using input

---
+++ celsuisConverter.js
@@ -0,0 +1,11 @@
+function tempConverter(celsuis){
+    var farenheit = (celsuis*9/5+32)
+    return farenheit;
+}
+
+var userInput = window.prompt("Enter your choiced degree celsuis to convert: ");
+var farenheitResult = tempConverter(float(userInput));
+var message = userInput+" degrees celsuis are "+farenheitResult+" degrees farenheit"
+console.log(message);
+
+
